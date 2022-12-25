a92ce61703aba74900174ad4a0d8c628a33bedfd
clarkbarrett101 <clarkbarrett101@gmail.com>
2022-12-24 21:53:36 -0700

    draft1

---
+++ todolist.js
@@ -0,0 +1,33 @@
+itemState = new Array;
+itemState[0] = false;
+complete=0;
+currentItem = 1;
+unfinished = "m-2 flex flex-col items-center border-2 border-slate-400 p-1 hover:bg-slate-400 hover:text-white text-slate-500";
+finished = "m-2 flex flex-col items-center border-2 hover:border-slate-400 p-1 bg-slate-400 text-white hover:text-slate-500"
+function createNewTask(){
+    console.log("new");
+    node = document.getElementById(0).cloneNode(true);
+    node.setAttribute("id",currentItem);
+    node.setAttribute("onclick","completeItem("+currentItem+")");
+    itemState[currentItem] = false;
+    document.getElementById("start").appendChild(node);
+    currentItem++;
+   setTotal();
+}
+function completeItem(id){
+    if(itemState[id]==false){
+    document.getElementById(id).setAttribute("class",finished);
+    itemState[id] = true;
+    complete++;
+}
+else
+{
+    document.getElementById(id).setAttribute("class",unfinished);
+    itemState[id]= false;
+    complete--;
+}
+setTotal();
+}
+function setTotal(){
+    document.getElementById("total").innerText = complete + " / "+currentItem;
+}
